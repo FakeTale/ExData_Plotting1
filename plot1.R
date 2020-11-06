e03835fef2e4230aa7b6ef528c6a005a2a3d40ee
FakeTale <35731039+FakeTale@users.noreply.github.com>
2020-11-06 15:10:44 +0000

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,6 +1,8 @@
 plot1 <- function(df) {
+  #use other function to read the data
   source('getData.R')
   df <- getData()
+  #Make the plot
   hist(df$Global_active_power, col='red', main = 'Global Active Power', xlab = 'Global Active Power (kilowatts)')
   dev.copy(png, file = "plot1.png")  
   dev.off()
