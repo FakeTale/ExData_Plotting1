8e1c36575f8fc37d92ea352da8e0c2de2c3c45da
FakeTale <35731039+FakeTale@users.noreply.github.com>
2020-11-06 15:11:59 +0000

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,6 +1,8 @@
 plot2 <- function(df) {
+  #Use function to get prepared data
   source('getData.R')
   df <- getData()
+  #Make the plot
   plot(df$datetime, df$Global_active_power, ylab = 'Global ctive Power (kilowatts)', xlab = '', type='l')
   dev.copy(png, file = "plot2.png")  
   dev.off()
